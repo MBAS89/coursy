c784913dc2a2c92333a74a289f93ab4a170e315e
MBAS89 <mohammedbadran1@hotmail.com>
2023-11-30 22:14:16 +0200

    continue working on the live meeting page

--- frontend/src/pages/Livemeeting.jsx
+++ frontend/src/pages/Livemeeting.jsx
@@ -12,6 +12,12 @@ import { RxDotFilled } from "react-icons/rx";
 import { GoSearch } from "react-icons/go";
 import { VscBellDot } from "react-icons/vsc";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdPerson } from "react-icons/md";
+import { ImPhoneHangUp } from "react-icons/im";
+import { FaMicrophone } from "react-icons/fa";
+import { BsFillCameraFill } from "react-icons/bs";
+
+import { IoCameraReverseSharp } from "react-icons/io5";
 export const Livemeeting = () => {
     return (
     <div>
@@ -61,7 +67,7 @@ export const Livemeeting = () => {
                                         <Link to="" className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700">
                                             <FaRegBookmark className='text-2xl'/>
                                             <span className="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white opacity-0 group-hover:opacity-100">
-                                            Save
+                                                Save
                                             </span>
                                         </Link>
                                     </li>
@@ -81,7 +87,7 @@ export const Livemeeting = () => {
                     </div>
                 </div>
             </section>
-            <div className='w-[100%] bg-pink-300'>
+            <div className='w-[100%]'>
                 <div className="h-16 w-full border-b-2 border-gray-100">
                     <div className="flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8">
                         <h2 className='text-3xl font-bold mr-5'>Room Meeting</h2>
@@ -107,10 +113,25 @@ export const Livemeeting = () => {
                         </div>
                     </div>
                 </div>
-
-                <div className='bg-green-300 flex h-screen'>
-                    <div className='w-[70%] bg-blue-300'></div>
-                    <div className='w-[30%] bg-orange-300'></div>
+                <div className='flex h-screen'>
+                    <div className='w-[70%]'>
+                        <div className='rounded-2xl mx-5 mt-5 w-[1250px] h-[500px] relative bg-purple-300'></div>
+                        <span className='absolute top-24 left-28 flex items-center gap-1 rounded-full px-4 py-px text-white bg-zinc-800'><MdPerson /> Rose</span>
+                        <div className='rounded-xl w-52 h-32 absolute top-24 left-[1100px] bg-yellow-300'></div>
+                        <span className='absolute top-48 left-[1110px] flex items-center gap-1 rounded-full px-4 py-px text-white bg-zinc-800'>Me</span>
+                        <ImPhoneHangUp className='absolute bottom-[380px] left-[800px] h-14 w-14 p-3 text-4xl text-white bg-green-500 rounded-full'/>
+                        <FaMicrophone className='absolute bottom-[380px] left-[700px] h-14 w-14 p-3 text-4xl text-white bg-zinc-700 opacity-75 rounded-full'/>
+                        <BsFillCameraFill className='absolute bottom-[380px] left-[600px] h-14 w-14 p-3 text-4xl text-white bg-zinc-700 opacity-75 rounded-full'/>
+                        <IoCameraReverseSharp className='absolute bottom-[380px] left-[500px] h-14 w-14 p-3 text-4xl text-white bg-zinc-700 opacity-75 rounded-full'/>
+                    </div>
+                    <div className='w-[30%] bg-orange-300'>
+                        <div className='mt-5 h-[840px] rounded-xl border-2 bg-pink-300 border-gray-300'>
+                            <div className='flex justify-between border-b border-zinc-100'>
+                                <h2 className=''>Live Chat</h2>
+                                <span className='flex items-center gap-1 px-2 rounded-md border border-zinc-100'><span className='font-semibold'>Group</span><MdKeyboardArrowDown /></span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
